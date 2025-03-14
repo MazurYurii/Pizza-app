cd5cda83d031117a61161f9f830418d5f9cd31c2
Yurii <yuriimazur1@gmail.com>
2025-03-14 15:51:57 +0200

    With async operations

--- pizza-app.js
+++ pizza-app.js
@@ -1,4 +1,4 @@
-//Callbacks hell
+//Pizza shop
 
 const storeRoom = {
     ingredients: ['грибы', 'салями', 'бекон', 'курица', 'кукуруза', 'томаты', 'ананасы', 'пармезан' ],
@@ -39,43 +39,36 @@ let preparePizza = (time, work) => {
 let prepareOrder = () => {
     console.log(`Приступаем к приготовлению пици используя следующие продукты: ${selectedIngredients.join(', ')}. Для основания используем ${selectedSous} соус.`);
 }
-
-
 let prepareIngredients = () => {
     console.log('Приготовление ингредиентов для пици окончено.');
 }
-
 let addSous = () => {
     console.log('На основу нанесен соус.');
 }
-
 let bake = () => {
     console.log('Выпекание пици окончено.');
 }
-
 let orderCompletion = () => {
     console.log('Порезали пицу и выдали клиенту. Приятного аппетита!');
 }
 
-getOrder(['курица', 'бекон', 'томаты', 'пармезан'], 'белый', preparePizza);
 
-preparePizza(1000, prepareOrder)
-.then(() => {
-    return preparePizza(2000, prepareIngredients);
-})
-.then(() => {
-    return preparePizza(1000, addSous);
-})
-.then(() => {
-    return preparePizza(3000, bake);
-})
-.then(() => {
-    return preparePizza(1000, orderCompletion);
-})
-.catch(error => {
-    console.log(error);
-})
-.finally(() => {
-    console.log('Хорошего Вам дня! Приходите к нам еще))');
-})
+//Async/await
+let orderFulFillment = async () => {
+    getOrder(['курица', 'бекон', 'томаты', 'пармезан'], 'белый', preparePizza);
+
+    try {
+        await preparePizza(1000, prepareOrder);
+        await preparePizza(2000, prepareIngredients);
+        await preparePizza(1000, addSous);
+        await preparePizza(3000, bake);
+        await preparePizza(1000, orderCompletion);
+    } catch(error) {
+        console.log(error);
+    } finally {
+        console.log('Хорошего Вам дня! Приходите к нам еще))');
+    }
+}
+
+orderFulFillment();
 
