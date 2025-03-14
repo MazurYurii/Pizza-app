6f7068af54a2d1689a3f98ab3a4fe7b508414acc
Yurii <yuriimazur1@gmail.com>
2025-03-14 12:12:12 +0200

    remove comments

--- pizza-app.js
+++ pizza-app.js
@@ -5,43 +5,6 @@ const storeRoom = {
     souses: ['белый', 'красный'],
 };
 
-// let orderFulFilment = (ingredients, sous, production) => {
-//     setTimeout(() => {
-//         const missingIngredients = ingredients.filter(ingredient => {
-//             return !storeRoom.ingredients.includes(ingredient);
-//         });
-
-//         if(missingIngredients.length) {
-//             console.log(`Извините, у нас сейчас закончиличь следующие продукты: ${missingIngredients.join(', ')}. Попробуйте применить другой сотав продуктов. `);
-//         } else {
-//             const ingredientsToAdd = storeRoom.ingredients.filter(ingredient => {
-//                 return ingredient.includes(ingredient);
-//             });
-//             console.log(`Приступаем к приготовлению пици используя следующие продукты: ${ingredientsToAdd.join(', ')}. Для основания используем ${sous} соус.`);
-//             production();
-//         }
-//     }, 1000);
-// }
-
-// let preparePizza = () => {
-//     setTimeout(() => {
-//         console.log('Приготовление ингредиентов для пици окончено.');
-
-//         setTimeout(() => {
-//             console.log('На основу нанесен соус.');
-
-//             setTimeout(() => {
-//                 console.log('Выпекание пици окончено.');
-
-//                 setTimeout(() => {
-//                     console.log('Порезали пицу и выдали клиенту. Приятного аппетита!');
-//                 }, 1000);
-//             }, 3000);
-//         }, 1000);
-//     }, 2000);
-// }
-
-// orderFulFilment(['курица', 'бекон', 'томаты', 'пармезан'], 'белый', preparePizza);
 
 let isAllGood = false;
 let missingIngredients = [];
